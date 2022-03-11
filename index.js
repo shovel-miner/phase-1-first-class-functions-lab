af985d1fbe4ac8e9238ebb3b9cd55991a82b61ed
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-03-11 05:21:05 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+    let newDrivers1 = drivers.slice(0,2)
+    return newDrivers1;
+};
+function returnLastTwoDrivers(drivers){
+    let newDrivers2 = drivers.slice(2,4)
+    return newDrivers2;
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function (fareMultiplier) {
+    return function(value){
+        return fareMultiplier * value;
+    }
+}
+function fareDoubler(numbers){
+    let sum = numbers * 2
+    return sum
+};
+function fareTripler(price){
+    let total = price * 3
+    return total
+};
+const selectDifferentDrivers = function (drivers,command){
+    return command(drivers);
+}
+
